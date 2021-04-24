ea015107aee6375e8d1141830354cae623fb0c56
oooo3994 <oooo3994@nate.com>
2021-04-25 00:59:51 +0900

    210425-12:59

--- src/components/ItemComponent.jsx
+++ src/components/ItemComponent.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import queryString from 'query-string';
 import ItemService from '../service/ItemService';
+import ReviewService from '../service/ReviewService';
 
 class ItemComponent extends Component{
     constructor(props){
@@ -8,7 +9,9 @@ class ItemComponent extends Component{
 
         super(props)
         this.state = {
-            itemInfo: [],
+            p_num: 1,
+            paging: {},
+            itemInfo: {},
             cateNo: query.cateNo,
             thisCateNo: query.thisCateNo,
             pdNo: query.pdNo,
@@ -20,7 +23,80 @@ class ItemComponent extends Component{
     componentDidMount(){
         ItemService.getCertainItem(this.state.pdNo, this.state.cateNo, this.state.thisCateNo).then( res => {
             this.setState({itemInfo: res.data});
-        })
+        });
+
+        ReviewService.getReviews(this.state.p_num, this.state.cateNo, this.state.thisCateNo, this.state.pdNo).then( res => {
+            this.setState({ 
+                p_num: res.data.pagingData.currentPageNum,
+                paging: res.data.pagingData,
+                reviews: res.data.list});
+        });
+    }
+
+    listBoard(p_num, cateNo, thisCateNo, pdNo) {
+        console.log("pageNum : "+ p_num);
+        ReviewService.getReviews(p_num, cateNo, thisCateNo, pdNo).then( res => {
+            console.log(res.data);
+            this.setState({ 
+                p_num: res.data.pagingData.currentPageNum,
+                paging: res.data.pagingData,
+                reviews: res.data.list});
+        });
+    }
+
+    viewPaging() {
+        const pageNums = [];
+
+        for (let i = this.state.paging.pageNumStart; i <= this.state.paging.pageNumEnd; i++ ) {
+            pageNums.push(i);
+        }
+
+        return (pageNums.map((page) => 
+        <li className="page-item" key={page.toString()} >
+            <a className="page-link" onClick = {() => this.listBoard(page, this.state.cateNo, this.state.thisCateNo, this.state.pdNo)}>{page}</a>
+        </li>
+        ));
+        
+    }
+
+    isPagingPrev(){
+        if (this.state.paging.prev) {
+            return (
+                <li className="page-item">
+                    <a className="page-link" onClick = {() => this.listBoard( (this.state.paging.currentPageNum - 1, this.state.cateNo, this.state.thisCateNo, this.state.pdNo) )} tabindex="-1">Previous</a>
+                </li>
+            );
+        }
+    }
+
+    isPagingNext(){
+        if (this.state.paging.next) {
+            return (
+                <li className="page-item">
+                    <a className="page-link" onClick = {() => this.listBoard( (this.state.paging.currentPageNum + 1, this.state.cateNo, this.state.thisCateNo, this.state.pdNo) )} tabIndex="-1">Next</a>
+                </li>
+            );
+        }
+    }
+
+    isMoveToFirstPage() {
+        if (this.state.p_num != 1) {
+            return (
+                <li className="page-item">
+                    <a className="page-link" onClick = {() => this.listBoard(1, this.state.cateNo, this.state.thisCateNo, this.state.pdNo)} tabIndex="-1">Move to First Page</a>
+                </li>
+            );
+        }
+    }
+
+    isMoveToLastPage() {
+        if (this.state.p_num != this.state.paging.pageNumCountTotal) {
+            return (
+                <li className="page-item">
+                    <a className="page-link" onClick = {() => this.listBoard( (this.state.paging.pageNumCountTotal,this.state.cateNo, this.state.thisCateNo, this.state.pdNo) )} tabIndex="-1">LastPage({this.state.paging.pageNumCountTotal})</a>
+                </li>
+            );
+        }
     }
 
 
@@ -34,28 +110,21 @@ class ItemComponent extends Component{
             <div className="clearfix"><h4 className="box-title pull-left"></h4>
             
             
-            <div>
-                {this.state.itemInfo.map(
-                    item=>
-                    <div key={item.pdNo, item.cateNo, item.thisCateNo}>
-                        <div style={{display:'inline', height:'400px'}}>
-                            <img src={item.pdImg} style={{display:'inline-block', width:'30%', height:'400px', alignItems:'flex-start', justifyContent:'space-around'}}/>
-
-                            <div style={{ display:'inline-block',margin:'0px 40px',width:'30%',height:'400px'}}>
-                                <div style={{ fontWeight:'bolder', fontSize:'23px'}}>{item.pdTitle}</div>
-                                <br/>
-                                <div style={{ fontWeight:'bolder', fontSize:'20px',color:'#ea4335'}}>{item.pdPrice}</div>
-                                    <button className="btn btn-primary waves-effect waves-light" style={{marginTop:'130px'}}>BUY NOW</button>
-                                    <button className="btn btn-primary waves-effect waves-light"style={{marginLeft:"10px",marginTop:'130px'}}>CART</button>
-                            </div>  
-                            <div style={{ display:'inline-block',margin:'0px 40px',width:'25%',height:'400px'}}>
-                                <div style={{ fontWeight:'bolder', fontSize:'23px'}}>리뷰 분석 결과</div>
-                            </div>
-                        </div>
-                  </div>
-                )}
+            
+            <div style={{display:'inline', height:'400px'}}>
+                <img src={this.state.itemInfo.pdImg} style={{display:'inline-block', width:'30%', height:'400px', alignItems:'flex-start', justifyContent:'space-around'}}/>
+                    <div style={{ display:'inline-block',margin:'0px 40px',width:'30%',height:'400px'}}>
+                        <div style={{ fontWeight:'bolder', fontSize:'23px'}}>{this.state.itemInfo.pdTitle}</div>
+                        <br/>
+                        <div style={{ fontWeight:'bolder', fontSize:'20px',color:'#ea4335'}}>{this.state.itemInfo.pdPrice}</div>
+                            <button className="btn btn-primary waves-effect waves-light" style={{marginTop:'130px'}}>BUY NOW</button>
+                            <button className="btn btn-primary waves-effect waves-light"style={{marginLeft:"10px",marginTop:'130px'}}>CART</button>
+                    </div>  
+                    <div style={{ display:'inline-block',margin:'0px 40px',width:'25%',height:'400px'}}>
+                        <div style={{ fontWeight:'bolder', fontSize:'23px'}}>리뷰 분석 결과</div>
+                    </div>
             </div>
-
+        
             <div className="table-responsive clearfix">
 			    <table className="table table-hover">
                     <thead>
@@ -68,22 +137,25 @@ class ItemComponent extends Component{
                         </tr>
                     </thead>
                     <tbody>
-                        
-                            <tr>
-                                <td>0</td>
-                                <td>5</td>
-                                <td>10000say</td>
-                                <td>ㄷㅈㅂㅈ</td>
-                                <td>21.04.19.</td>
-                               
-                        </tr>
+                        {
+                        this.state.reviews.map(
+                            review => 
+                            <tr key = {review.reviewNo, review.pdNo, review.subcateNo, review.cateNo}>
+                                <td> {review.reviewNo} </td>
+                                <td> {review.star} </td>
+                                <td> {review.customerId} </td>
+                                <td> {review.review} </td>
+                                <td> {review.reviewDate} </td>
+                            </tr>
+                        )
+                        }
                     </tbody>
                 </table>
             </div>
-
-                {/* <div className ="row">
+            <div className ="row">
                 <nav aria-label="Page navigation example">
                     <ul className="pagination justify-content-center">
+
                         {
                             this.isMoveToFirstPage()
                         }
@@ -101,8 +173,8 @@ class ItemComponent extends Component{
                         }
                     </ul>
                 </nav>
-            </div> */}
-               
+            </div>
+
             
 
             </div>
