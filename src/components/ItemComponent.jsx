e7eb635dff208940d0146ab7494aaed7b83af037
say <sshee0123@naver.com>
2021-05-16 15:07:20 +0900

    210516 git 수정사항 정리

--- src/components/ItemComponent.jsx
+++ src/components/ItemComponent.jsx
@@ -1,204 +1,10 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import queryString from 'query-string';
-import ItemService from '../service/ItemService';
-import ReviewService from '../service/ReviewService';
-
-class ItemComponent extends Component{
-    constructor(props){
-        const query = queryString.parse(window.location.search);
-
-        super(props)
-        this.state = {
-            p_num: 1,
-            paging: {},
-            itemInfo: {},
-            cateNo: query.cateNo,
-            thisCateNo: query.subcateNo,
-            pdNo: query.pdNo,
-            reviews: [],  //리뷰목록
-            reviewResult: []  //리뷰키워드그래프 데이터타입 아직모름.
-        }
-    }
-
-    componentDidMount(){
-        ItemService.getCertainItem(this.state.pdNo, this.state.cateNo, this.state.thisCateNo).then( res => {
-            this.setState({itemInfo: res.data});
-        });
-
-        ReviewService.getReviews(this.state.p_num, this.state.cateNo, this.state.thisCateNo, this.state.pdNo).then( res => {
-            this.setState({ 
-                p_num: res.data.pagingData.currentPageNum,
-                paging: res.data.pagingData,
-                reviews: res.data.list});
-        });
-    }
-
-    listBoard(p_num, cateNo, thisCateNo, pdNo) {
-        console.log("pageNum : "+ p_num);
-        ReviewService.getReviews(p_num, cateNo, thisCateNo, pdNo).then( res => {
-            console.log(res.data);
-            this.setState({ 
-                p_num: res.data.pagingData.currentPageNum,
-                paging: res.data.pagingData,
-                reviews: res.data.list});
-        });
-    }
-
-    viewPaging() {
-        const pageNums = [];
-
-        for (let i = this.state.paging.pageNumStart; i <= this.state.paging.pageNumEnd; i++ ) {
-            pageNums.push(i);
-        }
-
-        return (pageNums.map((page) => 
-        <li className="page-item" key={page.toString()} >
-            <a className="page-link" onClick = {() => this.listBoard(page, this.state.cateNo, this.state.thisCateNo, this.state.pdNo)}>{page}</a>
-        </li>
-        ));
-        
-    }
-
-    isPagingPrev(){
-        if (this.state.paging.prev) {
-            return (
-                <li className="page-item">
-                    <a className="page-link" onClick = {() => this.listBoard( (this.state.paging.currentPageNum - 1, this.state.cateNo, this.state.thisCateNo, this.state.pdNo) )} tabindex="-1">Previous</a>
-                </li>
-            );
-        }
-    }
-
-    isPagingNext(){
-        if (this.state.paging.next) {
-            return (
-                <li className="page-item">
-                    <a className="page-link" onClick = {() => this.listBoard( (this.state.paging.currentPageNum + 1, this.state.cateNo, this.state.thisCateNo, this.state.pdNo) )} tabIndex="-1">Next</a>
-                </li>
-            );
-        }
-    }
-
-    isMoveToFirstPage() {
-        if (this.state.p_num != 1) {
-            return (
-                <li className="page-item">
-                    <a className="page-link" onClick = {() => this.listBoard(1, this.state.cateNo, this.state.thisCateNo, this.state.pdNo)} tabIndex="-1">Move to First Page</a>
-                </li>
-            );
-        }
-    }
-
-    isMoveToLastPage() {
-        if (this.state.p_num != this.state.paging.pageNumCountTotal) {
-            return (
-                <li className="page-item">
-                    <a className="page-link" onClick = {() => this.listBoard( (this.state.paging.pageNumCountTotal,this.state.cateNo, this.state.thisCateNo, this.state.pdNo) )} tabIndex="-1">LastPage({this.state.paging.pageNumCountTotal})</a>
-                </li>
-            );
-        }
-    }
-
-
-    render(){
-     
-        return (
-            <div className="main-content"> 
-            <div className="row row-inline-block small-spacing">
-            <div className="col-xs-12">                   
-            <div className="box-content">
-            <div className="clearfix"><h4 className="box-title pull-left"></h4>
-            
-            
-            
-            <div style={{display:'inline', height:'400px'}}>
-                <img src={this.state.itemInfo.pdImg} style={{display:'inline-block', width:'30%', height:'400px', alignItems:'flex-start', justifyContent:'space-around'}}/>
-                    <div style={{ display:'inline-block',margin:'0px 40px',width:'30%',height:'400px'}}>
-                        <div style={{ fontWeight:'bolder', fontSize:'23px'}}>{this.state.itemInfo.pdTitle}</div>
-                        <br/>
-                        <div style={{ fontWeight:'bolder', fontSize:'20px',color:'#ea4335'}}>{this.state.itemInfo.pdPrice}</div>
-                            <button className="btn btn-primary waves-effect waves-light" style={{marginTop:'130px'}}>BUY NOW</button>
-                            <button className="btn btn-primary waves-effect waves-light"style={{marginLeft:"10px",marginTop:'130px'}}>CART</button>
-                    </div>  
-                    <div style={{ display:'inline-block',margin:'0px 40px',width:'25%',height:'400px'}}>
-                        <div style={{ fontWeight:'bolder', fontSize:'23px'}}>리뷰 분석 결과</div>
-                    </div>
-            </div>
-        
-            <div className="table-responsive clearfix">
-             <table className="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>리뷰No.</th>
-                            <th>별점</th>
-                            <th>아이디</th>
-                            <th>리뷰</th>
-                            <th>날짜</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                        this.state.reviews.map(
-                            review => 
-                            <tr key = {review.reviewNo, review.pdNo, review.subcateNo, review.cateNo}>
-                                <td> {review.reviewNo} </td>
-                                <td> {review.star} </td>
-                                <td> {review.customerId} </td>
-                                <td> {review.review} </td>
-                                <td> {review.reviewDate} </td>
-                            </tr>
-                        )
-                        }
-                    </tbody>
-                </table>
-            </div>
-            <div className ="row">
-                <nav aria-label="Page navigation example">
-                    <ul className="pagination justify-content-center">
-
-                        {
-                            this.isMoveToFirstPage()
-                        }
-                        {
-                            this.isPagingPrev()
-                        }
-                        {
-                            this.viewPaging()
-                        }
-                        {
-                            this.isPagingNext()
-                        }
-                        {
-                            this.isMoveToLastPage()
-                        }
-                    </ul>
-                </nav>
-            </div>
-
-            
-
-            </div>
-            </div>
-            </div>
-            </div>
-            </div>   
-        );
-    }
-
-}
-
-=======
 import React, { Component } from 'react';
 import queryString from 'query-string';
 import ItemService from '../service/ItemService';
 import ReviewService from '../service/ReviewService';
 import LikeService from '../service/LikeService';
 import MemberService from '../service/MemberService';
-<<<<<<< HEAD
 import HashtagService from '../service/HashtagService';
-=======
->>>>>>> 8b10a9616ebad19dfbffa49177514e247324b15c
 
 class ItemComponent extends Component{
     constructor(props){
@@ -425,7 +231,6 @@ class ItemComponent extends Component{
                     <button className="btn btn-primary waves-effect waves-light"style={{marginLeft:"10px", left:'13em', bottom:'3em', position:'absolute'}}>CART</button>
                     
                 </div>
-<<<<<<< HEAD
 
                 <div style={{padding:'3em 5em', textAlign:'center'}}>
                 <div >
@@ -454,15 +259,6 @@ class ItemComponent extends Component{
             <div className="nounhashbtn">#&nbsp;재구매</div>
             <div className="nounhashbtn">#&nbsp;조립</div>
           
-
-=======
-                <div style={{padding:'3em'}}>
-                    <div style={{ fontSize:'large', color:'black'}}>소비자 리뷰 분석 결과</div>   
-                </div>  
-                    
-            </div>
-            <br/><br/>
->>>>>>> 8b10a9616ebad19dfbffa49177514e247324b15c
             {
                 (this.state.reviews) && ( // 리뷰가 있으면 리뷰 뽑고
                     <div>
@@ -526,14 +322,6 @@ class ItemComponent extends Component{
                 )
             }
         
-            
-
-<<<<<<< HEAD
-=======
-            
-
-            
->>>>>>> 8b10a9616ebad19dfbffa49177514e247324b15c
             </div>
             </div>
             </div>
@@ -543,5 +331,4 @@ class ItemComponent extends Component{
 
 }
 
->>>>>>> 8c4fad14df0e9bbeb576640f19a25503ec180052
 export default ItemComponent;
--- src/service/ItemService.js
+++ src/service/ItemService.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-import axios from 'axios'; 
-import authHeader from './auth-header';
-
-
-const ITEM_API_BASE_URL = "http://localhost:8080/api/items"; 
-
-class ItemService {
-    getAllItems(cateNo){
-        if(cateNo=="거실가구"){
-            return axios.get(ITEM_API_BASE_URL + "/list/allLivingroom", { headers: authHeader() });
-        }
-        else if(cateNo=="침실가구"){
-            return axios.get(ITEM_API_BASE_URL + "/list/all Bedroom", { headers: authHeader() });
-        }
-        else if(cateNo=="서재/사무용가구"){
-            return axios.get(ITEM_API_BASE_URL + "/list/allLibrary", { headers: authHeader() });
-        }
-        else if(cateNo=="수납가구"){
-            return axios.get(ITEM_API_BASE_URL + "/list/allStorage", { headers: authHeader() });
-        }
-        else { // 주방가구
-            return axios.get(ITEM_API_BASE_URL + "/list/allKitchen", { headers: authHeader() });
-        }
-    }
-
-    getCertainItems(subcateNo,category){
-       return axios.get(ITEM_API_BASE_URL+"/list/"+category+"?subcateNo="+subcateNo, { headers: authHeader() });
-
-    }
-
-    getCertainItem(pdNo,cateNo,subcateNo){
-        return axios.get(ITEM_API_BASE_URL + "/getItem?pdNo="+pdNo+"&cateNo="+cateNo+"&subcateNo="+subcateNo, { headers: authHeader() });
-    }
-
-    // //카테고리 상관 x
-    // searchItems(searchKeyword){
-    //     return axios.get(ITEM_API_BASE_URL+"/searchAll?keyword="+searchKeyword, { headers:authHeader()});
-    // }
-
-    // //카테고리 별 검색
-    // searchCateItems(searchKeyword,cateNo,thisCateNo){
-    //     return axios.get(ITEM_API_BASE_URL+"/search?keyword="+searchKeyword+"&category="+cateNo+"&thisCate="+thisCateNo,{headers:authHeader()});
-    // }
-
-}
-
-=======
 import axios from 'axios'; 
 import authHeader from './auth-header';
 
@@ -215,5 +167,4 @@ class ItemService {
     
 }
 
->>>>>>> 8c4fad14df0e9bbeb576640f19a25503ec180052
 export default new ItemService();
