7f5e6d9004bb043f6f963f7e961dece9e8ded7a2
oooo3994 <oooo3994@nate.com>
2021-06-08 03:00:00 +0900

    210608 proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,14 +3,14 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         '/api/', proxy({
-            target: 'http://localhost:8080/',
+            target: 'http://54.180.2.178:8080/',
             changeOrigin: true,
         })
     );
     app.use(
         '/rec/', proxy({
             Headers:{'Access-Control-Allow-Origin' : '*'},
-            target: 'http://localhost:5000/',
+            target: 'http://0.0.0.0:5000/',
             changeOrigin: true,
         })
     );
