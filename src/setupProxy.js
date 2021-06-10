e93d5b7821e36a1f2dcba84625de5eaa42a7d95b
oooo3994 <oooo3994@nate.com>
2021-06-11 02:08:16 +0900

    210611 front

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         '/api/', proxy({
-            //target: 'http://3.37.87.98:8080/',
-            target: 'http://127.0.0.1:8080/',
+            target: 'http://3.37.87.98:8080/',
             changeOrigin: true,
         })
     );
